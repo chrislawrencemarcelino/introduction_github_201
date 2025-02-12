b82c2038307e2efe43ffb19f64c5fb8ed24fda5b
COMLAB <clmarcelino23-0293@cca.edu.ph>
2025-02-12 18:14:07 +0800

    index
    awkngksnakgsa

---
+++ 2index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>songs</title>
+</head>
+<body style="text-align: center;">
+    <h1>luther</h1>
+
+    <img src="ring.jpg" alt="ring image"
+    />
+    <div>
+        <p>If this world were mine</p>
+        <p>Hey, Roman numeral seven, bae, drop it like it's hot</p>
+        <p>If this world was mine, I'd take your dreams and make 'em multiply</p>
+        <p>If this world was mine, I'd take your enemies in front of God</p>
+        <p>Introduce 'em to that light, hit them strictly with that fire</p>    
+        <p>Fah-fah, fah-fah-fah, fah-fah, fah</p>
+    </div>
+</body>
+</html>
